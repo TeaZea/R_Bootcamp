553976452b227c6d9732bea65672978f2bd0c5fd
Stefano <sena.stefano@gmail.com>
2023-01-24 07:14:48 -0500

    use of variable lesson

---
+++ Section1/using_variables.R
@@ -0,0 +1,21 @@
+A <- 10
+B <- 5
+
+C <- A + B
+C
+
+var1 <- 2.5
+var2 <- 4
+# Highlight + run (ctrl +Enter) to execute multiple lines
+
+result <- var1 / var2
+result
+
+answer <- sqrt(var2)
+answer
+
+# Character Operations
+greeting <- "Hello"
+name <- "Bob"
+message <- paste(greeting, name)
+message
