9762646ac45b6c7aafdf9fb7d2324d349713a5ba
Stefano <sena.stefano@gmail.com>
2023-01-24 07:05:43 -0500

    Datatypes

---
+++ Section1/section1.R
@@ -0,0 +1,28 @@
+# Int
+x <- 2L
+typeof(x)
+
+# Double
+y <- 2.5
+typeof(y)
+
+# All operations are done via 'doubles' by default
+
+x <- 2
+typeof(x)
+
+# Reasigned variable, changed to 'double' by default (2.0)
+
+# Complex
+z <- 3 + 2i
+typeof(z)
+
+# Character
+a <- "h"
+typeof(a)
+
+# Logical
+q <- T
+typeof(q)
+q2 <- FALSE
+typeof(q2)
